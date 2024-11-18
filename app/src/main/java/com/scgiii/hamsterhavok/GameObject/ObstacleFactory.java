b3be0b8453f9de47335e73d43e6981f6f9db9ff3
Brittney <brittney.meza3@gmail.com>
2024-11-17 19:27:52 -0500

    not perfect implementation of additional obstacles

--- app/src/main/java/com/scgiii/hamsterhavok/GameObject/ObstacleFactory.java
+++ app/src/main/java/com/scgiii/hamsterhavok/GameObject/ObstacleFactory.java
@@ -24,7 +24,7 @@ public class ObstacleFactory {
         float spawnX = isFalling ? random.nextFloat() * screenWidth : screenWidth + 50; // Spawn above screen for falling
         float spawnY = isFalling ? -50 : screenHeight - 100; // Off-screen for falling, ground level otherwise
 
-        int obstacleType = random.nextInt(3); // Select obstacle type
+        int obstacleType = random.nextInt(4); // Select obstacle type
         switch (obstacleType) {
             case 0:
                 bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.roomba);
@@ -32,11 +32,43 @@ public class ObstacleFactory {
             case 1:
                 bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.books);
                 break;
+            case 2:
+                bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.baby);
+                bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.bowling_ball);
+                bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.bomb);
+                break;
+            case 3:
+                bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.clothes);
+                bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.pillow);
+                bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.basketball);
+                break;
             default:
                 bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.cat);
+                bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.microwave);
+                bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.chair);
+                bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.brick);
         }
 
-        bitmap = Bitmap.createScaledBitmap(bitmap, bitmap.getWidth() / 8, bitmap.getHeight() / 8, true);
+        // This is so that the scaling for the obstacles can be done dynamically.
+        // The standard is 15% of the og size and then the if and else-if block changes specifics
+        float scaleFactor = 0.15f;
+
+        if (obstacleType == 2){
+            scaleFactor = 0.45f;
+        } else if (obstacleType == 3) {
+            scaleFactor = 0.25f;
+        } else if (obstacleType == 2) {
+            scaleFactor = 0.35f;
+        } else if (obstacleType == 1) {
+            scaleFactor = 0.45f;
+        }
+
+        // This gets the new size based on its scaleFactor
+        int newWidth = (int) (bitmap.getWidth() * scaleFactor);
+        int newHeight= (int) (bitmap.getHeight() * scaleFactor);
+
+
+        bitmap = Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, true);
 
         float initialSpeed = isFalling ? 300 : Background.scrollSpeed;
 
