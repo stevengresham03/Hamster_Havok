3d74995a47df4bfeeec351651c3eaf4820e044bc
Brittney <brittney.meza3@gmail.com>
2024-11-18 00:18:37 -0500

    updated pngs and the start screen

--- app/src/main/java/com/scgiii/hamsterhavok/StartActivity.java
+++ app/src/main/java/com/scgiii/hamsterhavok/StartActivity.java
@@ -2,7 +2,6 @@ package com.scgiii.hamsterhavok;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.widget.ImageButton;
